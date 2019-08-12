81239cf0957ddb2fbf94974458347daa539af152
Isuperhero1 <49760771+Isuperhero1@users.noreply.github.com>
2019-08-12 21:20:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("610534245802442755
+let channel =     client.channels.get("610534245802442755")
 channel.send(`Seuih8a9shd9asf8sdfhsd8fh9dfdfdufhu9sdfhsd9fsdfhsdfhdsfuhdsu9fdfhusdhf9s`);
 }, 30)
 })
