96c9a48e38c71879ce45e7afff02fbf22a6b86ab
Isuperhero1 <49760771+Isuperhero1@users.noreply.github.com>
2019-08-12 21:12:49 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("610534245802442755
+channel.send(`Seuih8a9shd9asf8sdfhsd8fh9dfdfdufhu9sdfhsd9fsdfhsdfhdsfuhdsu9fdfhusdhf9s`);
+}, 30)
+})
+
+client.login(NTIzMTI0ODA5MTgyNDc4MzM2.XU8bTg.lnAEDrC6c3vN85i9ZjFYLDfL2PQ);
